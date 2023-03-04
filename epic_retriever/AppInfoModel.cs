b2ac47dbedb1eaf40789f1437aab9b2fc1913683
Nemirtingas <nemirtingas@noreply.com>
2023-03-04 13:04:30 +0100

    Sort Dlcs to have less changes per update.

---
+++ epic_retriever/AppInfoModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace epic_retriever
+{
+    internal class DlcInfoModel
+    {
+        public string Name { get; set; }
+        public string ItemId { get; set; }
+    }
+
+    internal class AppInfoModel
+    {
+        public string Name { get; set; }
+        public string AppId { get; set; }
+        public string Namespace { get; set; }
+        public string ItemId { get; set; }
+        public string ImageUrl { get; set; }
+        public List<string> Releases { get; set; }
+        public SortedDictionary<string, DlcInfoModel> Dlcs { get; set; }
+    }
+}
